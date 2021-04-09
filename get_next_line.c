da28afafe36f648393d0405eb5754944da091239
Aloïs <aloisebrechts@gmail.com>
2021-04-09 15:53:02 +0200

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,20 @@
 /*   By: asebrech <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 14:06:32 by asebrech          #+#    #+#             */
-/*   Updated: 2021/04/08 14:07:55 by asebrech         ###   ########.fr       */
+/*   Updated: 2021/04/09 15:51:38 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#define BUFFER_SIZE 4096
+
 int	get_next_line(int fd, char **line)
 {
-	
+	int		ret;
+	char	buf[BUFFER_SIZE + 1];
+
+	ret = read(fd, buf, BUFFER_SIZE);
+	buf[ret] = '\0';
+	return (ret);
 }
---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+typedef struct	t_fd
+{
+	int	fd1;
+	int	fd2;
+}				s_fd;
+
+s_fd	*ft_open(void)
+{
+	s_fd	*fds;
+
+	fds = calloc(1, sizeof(s_fd));
+	fds->fd1 = open("StarWars2.txt", O_RDONLY);
+	if (fds->fd1 == -1)
+		printf("open() failed : %d\n", fds->fd1);
+	else
+		printf("open StarWars2.txt : %d\n", fds->fd1);
+		
+	fds->fd2 = open("StarWars3.txt", O_RDONLY);
+	if (fds->fd2 == -1)
+		printf("open() failed : %d\n", fds->fd2);
+	else
+		printf("open StarWras3.txt : %d\n", fds->fd2);
+	return (fds);
+}
+
+void	*ft_close(s_fd *fds)
+{
+	close(fds->fd1);
+	if (fds->fd1 == -1)
+		printf("close() failed\n");
+	else
+		printf("close() worked\n");
+	close(fds->fd2);
+	if (fds->fd2 == -1)
+		printf("close() failed\n");
+	else
+		printf("close() worked\n");
+}
+
+int	main()
+{
+	s_fd	*fds;
+	int		ret;
+	char	**line;
+
+	line = NULL;
+	fds = ft_open();
+	ret = get_next_line(fds->fd1, line);
+	printf("next line : %d\n", ret);
+	ft_close(fds);
+
+	return (0);
+}
